3bdfcff5b176c9649d8e2cad4d45883b28ec93fe
ashish20013 <ashishky892@gmail.com>
2023-05-09 10:15:54 +0530

    roshan 8 may css

--- src/Componentes/atom/content.js
+++ src/Componentes/atom/content.js
@@ -2,20 +2,15 @@
 
 const Content = ()=>{
     return(
-        <p>
-            Residential Real Estate
-This type of real estate refers to properties that are designed and used for residential purposes. These can include single-family homes, apartments, condominiums, townhouses, and vacation homes. Residential real estate can be either newly constructed or previously owned.
-
-Commercial Real Estate
-Commercial real estate includes properties that are used for business purposes. This can include office buildings, retail spaces, hotels, malls, restaurants, and educational centers. Commercial properties can be owned or leased, and are often purchased as investments with the intention of generating income.
-
-Industrial Real Estate
-Industrial real estate refers to properties that are used for manufacturing, research, development, warehousing, and other industrial purposes. These properties can include factories, warehouses, distribution centers, and research facilities.
-
-Vacant Land
-This type of real estate refers to undeveloped land that has not been improved or used for any specific purpose. Vacant land can be used for a variety of purposes, such as farming, ranching, recreation, conservation, or future development.
-
-It is important to note that some real estate businesses may specialize in one type of real estate, while others may work across multiple types.
+        <p>Develop a Comprehensive Business Plan
+        Every business needs a plan. It helps you gain better clarity about what you mean to achieve. Do you want to start a real estate business to increase your income? Or are you looking for a career change? Or maybe it’s something you want to do on the side. Your reason will affect your business because real estate is a round-the-clock venture. You need to be available at all times for your client. Understand why you are starting the business.
+        
+        Next, you need the actual plan—your business plan. It gives you direction and helps you and your potential investors understand your goals and the milestones you hope to achieve. A properly drafted business plan helps you get loans from the bank if you really need any.
+        
+        Conduct Thorough Market Research
+        Conduct market research. There may be a rising need for green homes, or perhaps the university in your city needs more residential spaces. Researching will help you decide the sector to specialize in. Educate yourself on the aspects of real estate. If you are completely new to this, consider finding a mentor to help you initially. He/she may aid you in finding contacts, giving you a running list of dos and don’ts, whom to target, and so on.
+        
+        Apart from choosing a type of real estate to deal with, you also need to decide your specialty, which can be anything like residential brokerage, land development, property management, etc. Select the specialty you have the most knowledge about. You can add to your job titles as you gain experience.
         </p>
     )
 };
